e94c995bf6581ac3865a2c4e472a328a99816447
Justina Petraityte <juste@rasa.com>
2018-06-09 22:06:17 +0200

    changed input

--- load_nlu_model.py
+++ load_nlu_model.py
@@ -3,7 +3,7 @@ from rasa_nlu.model import Metadata, Interpreter
 def load_model():
 	# where `model_directory points to the folder the model is persisted in`
 	interpreter = Interpreter.load('models/current/nlu_model')
-	print(interpreter.parse("I have just arrived in Berlin. Can you suggest any cool meetups I could attend?"))
+	print(interpreter.parse("Yes. Can you give me suggestions on how to get there?"))
 	
 if __name__ == '__main__':
 	load_model()
